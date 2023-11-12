b81fbc056a0811a3eac792996fb7dfddc31ecc52
ngirimana <chadrackngirimana@gmail.com>
2023-11-12 21:23:16 +0200

    validations

--- src/main/java/com/springbot/blog/payload/PostDto.java
+++ src/main/java/com/springbot/blog/payload/PostDto.java
@@ -1,5 +1,8 @@
 package com.springbot.blog.payload;
 
+
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 import java.util.Set;
@@ -7,8 +10,13 @@ import java.util.Set;
 @Data
 public class PostDto {
     private Long id;
+    @NotEmpty(message = "Title cannot be empty")
+    @Size(min = 2, message = "Title should have at least 2 characters")
     private String title;
+    @NotEmpty(message = "Description cannot be empty")
+    @Size(min = 10, message = "Description should have at least 10 characters")
     private String description;
+    @NotEmpty(message = "Content cannot be empty")
     private String content;
     private Set<CommentDto> comments;
 }

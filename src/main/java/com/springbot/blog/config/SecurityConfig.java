6709616980e02ef88bec73bfa0fbe992056e34eb
ngirimana <chadrackngirimana@gmail.com>
2023-11-12 23:25:21 +0200

    basic spring security

---
+++ src/main/java/com/springbot/blog/config/SecurityConfig.java
@@ -0,0 +1,21 @@
+package com.springbot.blog.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+         http.csrf(csrf -> csrf.disable()).authorizeHttpRequests((authorize) -> authorize
+                .anyRequest().authenticated()
+        ).httpBasic(Customizer.withDefaults());
+
+        return http.build();
+    }
+}

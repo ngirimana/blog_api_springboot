aa736ee6e58b703e8771be2361ff6f2db0d8ae85
ngirimana <chadrackngirimana@gmail.com>
2023-11-14 10:22:18 +0200

    basic auth and in memory

--- src/main/java/com/springbot/blog/config/SecurityConfig.java
+++ src/main/java/com/springbot/blog/config/SecurityConfig.java
@@ -5,17 +5,43 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
+
+    public static PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
-         http.csrf(csrf -> csrf.disable()).authorizeHttpRequests((authorize) -> authorize
+         http.csrf(csrf ->  csrf.disable()).authorizeHttpRequests((authorize) -> authorize
                 .anyRequest().authenticated()
         ).httpBasic(Customizer.withDefaults());
 
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails safari = User.builder()
+                .username("safari")
+                .password(passwordEncoder().encode( "1234"))
+                .roles("USER")
+                .build();
+        UserDetails admin= User.builder()
+                .username("admin")
+                .password(passwordEncoder().encode( "admin"))
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(safari, admin);
     }
 }

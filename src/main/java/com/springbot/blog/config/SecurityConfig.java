c1179f41eea3408f4fb47706ffeb4613086eb401
ngirimana <chadrackngirimana@gmail.com>
2023-11-15 09:03:05 +0200

    adding db authentication

--- src/main/java/com/springbot/blog/config/SecurityConfig.java
+++ src/main/java/com/springbot/blog/config/SecurityConfig.java
@@ -1,10 +1,11 @@
 package com.springbot.blog.config;
 
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
@@ -19,34 +20,49 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableMethodSecurity
 public class SecurityConfig {
 
+    private UserDetailsService userDetailsService;
+
+    SecurityConfig(UserDetailsService userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
+
     @Bean
     public static PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+
+        return configuration.getAuthenticationManager();
+    }
+
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
-         http.csrf(csrf ->  csrf.disable()).authorizeHttpRequests((authorize) -> authorize
-//                .anyRequest().authenticated()
-                         .requestMatchers(HttpMethod.GET, "/api/**").permitAll().anyRequest().authenticated()
-        ).httpBasic(Customizer.withDefaults());
+        http.csrf(csrf -> csrf.disable()).authorizeHttpRequests((authorize) -> authorize
+                        // .anyRequest().authenticated()
+                        .requestMatchers(HttpMethod.GET, "/api/**").permitAll().anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults());
 
         return http.build();
     }
 
-    @Bean
-    public UserDetailsService userDetailsService() {
-        UserDetails safari = User.builder()
-                .username("safari")
-                .password(passwordEncoder().encode( "1234"))
-                .roles("USER")
-                .build();
-        UserDetails admin= User.builder()
-                .username("admin")
-                .password(passwordEncoder().encode( "admin"))
-                .roles("ADMIN")
-                .build();
+    // in memory authentication
 
-        return new InMemoryUserDetailsManager(safari, admin);
-    }
+//    @Bean
+//    public UserDetailsService userDetailsService() {
+//        UserDetails safari = User.builder()
+//                .username("safari")
+//                .password(passwordEncoder().encode("1234"))
+//                .roles("USER")
+//                .build();
+//        UserDetails admin = User.builder()
+//                .username("admin")
+//                .password(passwordEncoder().encode("admin"))
+//                .roles("ADMIN")
+//                .build();
+//
+//        return new InMemoryUserDetailsManager(safari, admin);
+//    }
 }

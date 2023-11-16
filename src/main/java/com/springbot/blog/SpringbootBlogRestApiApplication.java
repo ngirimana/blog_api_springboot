754d5ca9a1a9a0ad7045929c7a44ef6499adfc35
ngirimana <schadran@andrew.cmu.edu>
2023-11-16 12:31:50 +0200

    Deploying to aws

--- src/main/java/com/springbot/blog/SpringbootBlogRestApiApplication.java
+++ src/main/java/com/springbot/blog/SpringbootBlogRestApiApplication.java
@@ -1,14 +1,18 @@
 package com.springbot.blog;
 
+import com.springbot.blog.repository.RoleRepository;
 import io.swagger.v3.oas.annotations.ExternalDocumentation;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.info.License;
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import com.springbot.blog.entity.Role;
 
 @SpringBootApplication
 @OpenAPIDefinition(
@@ -31,7 +35,7 @@ import org.springframework.context.annotation.Bean;
                 url = "https://github.com/ngirimana/blog_api_springboot"
         )
 )
-public class SpringbootBlogRestApiApplication {
+public class SpringbootBlogRestApiApplication implements CommandLineRunner {
     @Bean
     public ModelMapper modelMapper() {
         return new ModelMapper();
@@ -39,5 +43,20 @@ public class SpringbootBlogRestApiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootBlogRestApiApplication.class, args);
+    }
+
+    @Autowired
+    private RoleRepository roleRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        Role adminRole = new Role();
+        adminRole.setName("ROLE_ADMIN");
+        roleRepository.save(adminRole);
+        Role userRole = new Role();
+        userRole.setName("ROLE_USER");
+        roleRepository.save(userRole);
+
+
     }
 }
